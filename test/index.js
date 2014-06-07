80f796f1fc98623a2bf3944deb8fd329eeb6afbd
franleplant <franleplant@gmail.com>
2014-06-07 16:25:46 -0300

    fix test trigger

--- test/index.js
+++ test/index.js
@@ -1,14 +1,15 @@
-var glob = require("glob");
+var glob = require('glob');
+var path = require('path');
 
-var path = './test/test_case_*.js';
+var globs = './test/test_case_*.js';
 
 // Simple harness to require all the test cases
-glob(path, function (err, file_names) {
+glob(globs, function (err, file_names) {
 	if (err) {
 		throw err;
 	}
 
 	file_names.forEach(function (file_name) {
-		require(file_name);
+		require(  './' + path.relative(__dirname, file_name)  );
 	});	
 });
