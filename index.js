32d656b38f47eb8e3f7ea07adf3bf9e84628b500
franleplant <franleplant@gmail.com>
2014-06-06 23:38:18 -0300

    improve inline docs

--- index.js
+++ index.js
@@ -11,32 +11,42 @@ module.exports = function (opts) {
 
     var less_str = '';
 
-    //Opts is the same as less opts plus some custom options
+    // Opts is the same as less opts plus some custom options not related with less
     opts = opts || {};
     opts.paths = [__dirname];
 
 
     return function (fileName) {
 
+        // Process only css or less files
         if (!/\.css$|\.less$/.test(fileName)) {
             return through();
         }
 
         var buffer = "";
 
+
+        //Save file path so that the Less parser can resolve
+        // imports
         opts.paths.push( path.dirname(fileName) );
 
+        //Write function: 
+        // buffer the entire file
  		function write (chunk) {
                 buffer += chunk.toString();
         }
 
+        //End function:
+        // Save and concatenate the less/css buffer into less_str
+        // Compile less_str with its options 
+        // (compile less is dobounced)
         function end () {
             less_str += buffer;
-
             compile_less(less_str, opts)
-
             this.queue(null);
         }
+
+
         return through(write, end);
     }   
 };
--- src/compile_less.js
+++ src/compile_less.js
@@ -1,7 +1,5 @@
 "use strict";
 
-
-
 var less = require('less');
 var path = require("path");
 var db = require('just-debounce');
@@ -9,13 +7,18 @@ var callback = require('callback-stream');
 var Readable = require('stream').Readable;
 
 
+// Create a debounced less compile function.
+// In this way the process will wait untill it gets all the less
+// or css files and only then compile it into css
+
+//Right now its debounced for 1 second, this can, and should be improved
 var debounced_compile = db(function compile(less_str, options) {
 
         var less_stream = new Readable;
         var stream;
 
 
-        //Defaults
+        //Default options
         options = options || {};
         options.syncImport = true;
 
@@ -28,19 +31,17 @@ var debounced_compile = db(function compile(less_str, options) {
 
         less.render(less_str, options, function (err, css) {
             if (err) {
-
-                // convert the keys so PluginError can read them
-                err.lineNumber = err.line;
-                err.fileName = err.filename;
-
-                // add a better error message
-                err.message = 'Less compile:' + err.message + ' in file ' + err.fileName + ' line no. ' + err.lineNumber;
-
-                throw err.message;
+                // Error message
+                throw 'Less compile:' + err.message + ' line number. ' + err.line;
             } else {
-
+                //Initialize the less_stream readable stream with the entire
+                // compiled css
                 less_stream.push(css);
                 less_stream.push(null);
+
+                // The following code implements the pipe interface
+                // It basically execute the pipe array of stream transform
+                // in order
 
                 stream = less_stream.pipe(options.pipe[0]);
 
@@ -49,6 +50,11 @@ var debounced_compile = db(function compile(less_str, options) {
                 };
 
 
+                // Finish and end events execute the callback
+                // Use it for advaced functionality (we use it for testing)
+                // Both events are being captured because sometimes
+                // it fires end and sometimes it fires finish
+                // I should go deeper into understanding why.
                 stream.on('finish', function () { 
                     options.cb(); 
                 });
@@ -61,9 +67,6 @@ var debounced_compile = db(function compile(less_str, options) {
         });
          
     }, 1000, false);
-
-
-
 
 
 module.exports = debounced_compile;
